56af06b7bdc5154b8262e36b9a1c16723c3d79e1
ruining-l <liurn233@163.com>
2022-05-10 09:35:42 +0800

    construct again successfully

--- functional_tests.py
+++ functional_tests.py
@@ -1,81 +1,63 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import unittest
 import time
+import unittest
 
-class NewVisitorTest(unittest.TestCase):
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-    
-    def tearDown(self):
-        self.browser.quit()
-    
-    def check_for_row_in_list_table(self,row_text):
-        table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_element_by_tag_name('tr')
-        self.assertIn(row_text,[row.text for row in rows])
+class NewVisitorTest(unittest.TestCase):		#（1）
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
-        self.browser.get('http://localhost:8000')
+       def setUp(self):	#(3)
+              self.browser = webdriver.Firefox()
 
-        # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name( 'h1' ).text
-        #header_text=self.browser.find_element(By,TAG_NAME,'h1').text
-        self.assertIn( 'To-Do', header_text)
+       def tearDown(self): #(3)
+              self.browser.quit()
 
-        # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id( 'id_new_item' )
-        # self.assertEqual(
-        #     inputbox.get_attribute( 'placeholder' ),
-        #     'Enter a to-do item'
-        # )
-        inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        # She types "Buy peacock feathers" into a text box (Edith's hobby
-        # is tying fly-fishing lures)
-        # inputbox.send_keys( 'Buy peacock feathers' )
+       def test_can_start_a_list_and_retrieve_it_later(self): #(2)
+              # Edith has heard about a cool new online to-do app. She goes
+              # to check out its homepage
+              self.browser.get('http://localhost:8000')
 
-        # when she hits enter,the page updates,and now the page lists
-        # "1:Buy peacock feathers"'as an item in a to-do list
-        # inputbox.send_keys(Keys.ENTER)
-        # time.sleep(1)
+              #She notices the page title and header mention to-do lists
+              self.assertIn('To-Do',self.browser.title)	#(4)
+              header_text = self.browser.find_element_by_tag_name('h1').text  #1
+              self.assertIn('To-Do',header_text)
 
-        # table = self.browser.find_element_by_id( 'id_list_table' )
-        # rows = table.find_elements_by_tag_name( 'tr' )
-        # self.assertTrue(
-        #     any( row.text == '1: Buy peacock feathers' for row in rows),
-        #     f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        #     )
-        # self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
-        # self.assertIn(
-        #     '2:Use peacock feathers to make a fly',
-        #     [row.text for row in rows]
-        # )
-        check_for_row_in_list_table(self,'2:Use peacock feathers to make a fly')
+#She is invited to enter a to-do item straight away
+              inputbox = self.browser.find_element_by_id('id_new_item')   #1
+              self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
 
-        # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail( 'Finish the test! ' )
+# She types "Buy peacock feathers" into a text box (Edith's hobby
+# is tying fly-fishing lures）
+              inputbox.send_keys('Buy peacock feathers')  #2
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
-
-# She is ivited to enter a to-do item staight away
-# She types "Buy peacock feathers" into a text box(Edith's hobby
-# is tying fly-fishing lures)
-
-# when she hits enter, the page updates, and now the page lists
+# when she hits enter，the page updates，and now the page lists
 # "1:Buy peacock feathers" as an item in a to-do list
+              inputbox.send_keys(Keys.ENTER)  #3
+              time.sleep(1)  #4
 
+              table=self.browser.find_element_by_id('id_list_table')
+              rows=table.find_elements_by_tag_name('tr')  #1
+#              self.assertTrue(any(row.text=='1:Buy peacock feathers' for row in rows),f"New to-do item did not appear in table. Contents were:\n{table.text}")
+              self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
 # There is still a text box inviting her to add another item. She
-# enters "Use peacock feathers to make a fly"(Edith is very methodical)
+# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+              inputbox = self.browser.find_element_by_id('id_new_item')
+              inputbox.send_keys('Use peacock feathers to make a fly')
+              inputbox.send_keys(Keys.ENTER)
+              time.sleep(1)
+# The page updates again，and now shows both items on her list
+              table = self.browser.find_element_by_id('id_list_table')
+              rows=table.find_elements_by_tag_name('tr')
+              self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+              self.assertIn('2:Use peacock feathers to make a fly',[row.text for row in rows])
+           
+              self.fail('Finish the test!')	#(5)
 
-# The page updates again, and now shows both items on her list
+if __name__ == '__main__': #(6)
+       unittest.main(warnings='ignore') #(7)
+
+
+
+
 
 # Edith wonders whether the site will remember her list. Then she sees
 # that the site has generated a unique URL for her -- there is some
@@ -83,4 +65,4 @@ if __name__ == '__main__':
 
 # She visits that URL - her to-do list is still there.
 
-# Satisfied, she goes back to sleep
+# Satisfied，she goes back to sleep
