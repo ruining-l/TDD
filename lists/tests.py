dd95a89ba44181b5ac19b58129f897a52364778d
ruining-l <liurn233@163.com>
2022-05-17 09:57:34 +0800

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -23,34 +23,32 @@ class ItemModelTest(TestCase):
         self.assertEqual(second_saved_item.text,'Item the second')
 
 class HomePageTest(TestCase):
-    # def test_root_url_resolve_to_home_page_view(self):
-    #     found = resolve('/')
-    #     self.assertEqual(found.func,home_page)
-
-    # def test_home_page_returns_correct_html(self):
-    #     # request = HttpRequest()# 1
-    #     # response = home_page(request) # 2
-    #     # html = response.content.decode('utf8')# 3
-    #     # # self.assertTrue(html.startswith('<html>'))# 4
-    #     # # self.assertIn('<title>To-Do lists</title>', html) # 5
-    #     # # self.assertTrue(html.endswith('</html>'))#4
-    #     # expected_html=render_to_string('home.html')
-    #     # self.assertEqual(html,expected_html)
-    #     response=self.client.get('/') #1
-
-    #     html = response.content.decode('utf8')#2
-    #     self.assertTrue(html.startswith('<html>'))#3
-    #     self.assertIn('<title>To-Do lists</title>', html)
-    #     self.assertTrue(html.strip().endswith('</html>'))
-
-    #     self.assertTemplateUsed(response,'wrong.html') #3
     def test_uses_home_template(self):
         response=self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
     
-    def test_can_sace_a_POST_request(self):
+    def test_can_save_a_POST_request(self):
         response =self.client.post('/',data={'item_text':'A new list item'})
-        self.assertIn('A new list item',response.content.decode())
-        self.assertTemplateUsed(response,'home.html')
 
+        self.assertEqual(Item.objects.count(),1)
+        new_item=Item.objects.first()
+        self.assertEqual(new_item.text,'A new list item')
+    
+    def test_redirects_after_POST(self):
+        response =self.client.post('/',data={'item_text':'A new list item'})
+        self.assertEqual(response.status_code,302)
+        self.assertEqual(response['location'],'/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(),0)
+
+    def test_display_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response=self.client.get('/')
+
+        self.assertIn('itemey 1',response.content.decode())
+        self.assertIn('itemey 2',response.content.decode())
 # Create your tests here.
