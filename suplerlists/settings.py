bcbfac8d6bac90abe7f08dcccafe2623657c16b3
ruining-l <liurn233@163.com>
2022-06-13 12:57:41 +0800

    Add gunicorn to virtualenv requirements

--- suplerlists/settings.py
+++ suplerlists/settings.py
@@ -123,3 +123,5 @@ STATIC_ROOT=os.path.join(BASE_DIR,'../static/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS=['http://47.111.120.83']
